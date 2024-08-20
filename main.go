73e8c135ce6ad14bb92fd8e563a6eadee4e91c28
breno <breno.nunes@segarante.com.br>
2024-08-19 21:06:35 -0300

    database setup

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+  "github.com/gofiber/fiber/v2"
+  "gorm.io/gorm"
+  "gorm.io/driver/mysql"
+  "fmt"
+)
+
+type Login struct {
+  gorm.Model
+  Identificator  string
+  PasswordHash string
+}
 
 func main() {
+  dsn := "root:mysql@tcp(127.0.0.1:3306)/mysqldb?charset=utf8mb4&parseTime=True&loc=Local"
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  if err != nil {
+    return
+  }
+
+  db.AutoMigrate(&Login{})
+
+  db.Create(&Login{Identificator: "D42", PasswordHash: "dwa"})
+
+  var login Login
+  db.First(&login, 1) // find product with integer primary key
+
+  fmt.Println(login)
+
   app := fiber.New()
 
   app.Static("/", "./frontend/src")
@@ -14,8 +40,4 @@ func main() {
 
   app.Listen(":3000")
 }
-
-
-
-
 
