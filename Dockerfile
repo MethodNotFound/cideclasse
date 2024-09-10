bd10e88a78a578980e940fd68d8cc419a7dae765
breno <breno.nunes@segarante.com.br>
2024-09-09 23:57:45 -0300

    most new docker file

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,23 @@
+FROM node:20 as build
+
+WORKDIR /build
+
+COPY . .
+
+RUN npm install
+
+RUN npm run build
+
 FROM docker.io/library/golang:1.23rc1-alpine AS lgo
 
 WORKDIR /back
+COPY . .
+RUN rm -rf ./backend/public
+COPY --from=build /build/public /back/public
 
-COPY ./backend .
-
-RUN go mod download
-
-RUN go build .
+RUN cd backend && go mod download
+RUN cd backend && go build .
 
 EXPOSE 3000
-
-ENTRYPOINT ["./cideclasse"]
-CMD ["./cideclasse"]
+ENTRYPOINT ["./backend/cideclasse"]
+CMD ["./backed/cideclasse"]
--- build.js
+++ build.js
@@ -1,32 +1,30 @@
 const esbuild = require("esbuild");
 const postCssPlugin = require('esbuild-style-plugin')
 
+let config = {
+  entryPoints: ["./src/javascript/main.js", "./src/style/style.css"],
+  outdir: 'public',
+  bundle: true,
+  minify: false,
+  plugins: [
+    postCssPlugin({
+      postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')],
+      },
+    }),
+  ]
+};
+
 async function watch() {
-  let ctx = await esbuild.context({
-    entryPoints: ["./src/javascript/main.js", "./src/style/style.css"],
-    outdir: 'public',
-    bundle: true,
-    minify: false,
-    plugins: [
-      postCssPlugin({
-        postcss: {
-          plugins: [require('tailwindcss'), require('autoprefixer')],
-        },
-      }),
-    ]
-  });
+  let ctx = await esbuild.context(config);
   await ctx.watch();
   console.log('Watching...');
 }
 
-// IMPORTANT: this call MUST NOT have an `await`.
-watch();
+if(!process.env.NOT_WATCH) {
+  watch();
+} else {
+  esbuild.build(config)
+}
 
-// If the call above had an `await`, Node would return
-// immediately and you would NOT have the watcher
-// running. Alternative, you could use an iife[1]:
-(async() => {
-  // The same code from the `watch` function above.
-  // Notice that it also doesn't have an `await` in
-  // front of it.
-})()
+
